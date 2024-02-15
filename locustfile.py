47df57a9beee398a4f625dde736a39435de6e807
sacOO7 <sachinshinde7676@gmail.com>
2024-02-15 21:47:13 +0530

    updated locustfile to include http flag for httpx

--- locustfile.py
+++ locustfile.py
@@ -5,18 +5,19 @@ from httpx_user import HttpxUser
 from request_user import RequestsUser
 
 
-class RequestsTestUser(RequestsUser):
-    pool_connections = 100
-    pool_maxsize = 100
-
-    @task
-    def fetch_ably_time(self):
-        self.client.get("/time")
-
-
-# class HttpxTestUser(HttpxUser):
-#     limits = httpx.Limits(max_keepalive_connections=150, max_connections=150, keepalive_expiry=120)
+# class RequestsTestUser(RequestsUser):
+#     pool_connections = 100
+#     pool_maxsize = 100
 #
 #     @task
 #     def fetch_ably_time(self):
 #         self.client.get("/time")
+
+
+class HttpxTestUser(HttpxUser):
+    limits = httpx.Limits(max_keepalive_connections=150, max_connections=150, keepalive_expiry=120)
+    http2 = True
+
+    @task
+    def fetch_ably_time(self):
+        self.client.get("/time")
